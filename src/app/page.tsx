5a32e94a35da850f5b03a4c1eb56f2f2024d3805
saur0w <sthapliyal085@gmail.com>
2026-02-23 21:08:03 +0530

    "GSAP Landing Animation"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,22 @@
 
 import styles from "./page.module.css";
 import Landing from "@/components/Landing/index";
+import Lenis from "lenis";
+import { useEffect } from "react";
 
 export default function Home() {
+    useEffect(() => {
+        const lenis = new Lenis();
+
+        function raf(time: number) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+
+        requestAnimationFrame(raf);
+
+        return () => lenis.destroy();
+    }, []);
   return (
     <div className={styles.page}>
       <Landing />
--- src/ui/Magnetic/index.tsx
+++ src/ui/Magnetic/index.tsx
@@ -6,6 +6,29 @@ import React, { useRef } from "react";
 
 export default function Magnetic({ children }: { children: React.ReactNode }) {
     const magneticRef = useRef<HTMLDivElement>(null);
+
+    useGSAP(() => {
+        const xTo = gsap.quickTo(magneticRef.current, "x", {duration: 1, ease: "elastic.out(1, 0.3)"});
+        const yTo = gsap.quickTo(magneticRef.current, "y", {duration: 1, ease: "elastic.out(1, 0.3)"});
+
+        const handleMouseMove = (e: MouseEvent) => {
+            const { clientX, clientY } = e;
+            const { height, width, left, top } = magneticRef.current!.getBoundingClientRect();
+            const x = clientX - (left + width / 2);
+            const y = clientY - (top + height / 2);
+            xTo(x);
+            yTo(y);
+        };
+
+        const handleMouseLeave = () => {
+            xTo(0);
+            yTo(0);
+        };
+
+        magneticRef.current?.addEventListener("mousemove", handleMouseMove);
+        magneticRef.current?.addEventListener("mouseleave", handleMouseLeave);
+
+    }, { scope: magneticRef });
     return (
         <div ref={magneticRef} style={{ display: "inline-block" }}>
             {children}
